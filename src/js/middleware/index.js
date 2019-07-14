3c261ddfda1d6836e3122d42620118e6327348b7
harrisonx <engcxia@gmail.com>
2019-07-14 16:56:47 -0400

    Clean up

--- src/js/middleware/index.js
+++ src/js/middleware/index.js
@@ -1,9 +1,9 @@
 import { ADD_ARTICLE } from '../constants/action-types'
 const forbiddenWords = ['spam', 'money']
+// Example of middleware
 export function forbiddenWordsMiddleware({ dispatch }) {
   return function(next) {
     return function(action) {
-      // do your stuff
       if (action.type === ADD_ARTICLE) {
         const foundWord = forbiddenWords.filter(word =>
           action.payload.title.includes(word)
