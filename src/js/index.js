4c42ed0442510fb39a33435e4dd291318f9197ab
harrisonx <engcxia@gmail.com>
2019-10-10 21:38:36 -0700

    Clean up and add react router

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store/index'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {Route, Switch, Redirect, BrowserRouter as Router} from 'react-router-dom'
+import store from './store/index.js'
 import App from './components/App.jsx'
+
+const defaultPath = '/'
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  // The target element might be either root or app,
-  // depending    on your development environment
-  // document.getElementById("app")
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path={defaultPath} component={App}/>
+                <Route path={`${defaultPath}example`} component={App}/>
+                <Redirect exact from="*" to={defaultPath}/>
+            </Switch>
+        </Router></Provider>,
+    document.getElementById('root'),
 )
